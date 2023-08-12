7bbbc636c32bdeba94df31f6c9bb0863a1afff37
Thakshila navodya watadenithanna <65487716+Thakshila-N@users.noreply.github.com>
2023-08-12 12:08:45 +0530

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import {Greet} from './Components/Greet'
+import Wellcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Wellcome />
     </div>
   );
 }
---
+++ src/Components/Welcome.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Wellcome extends Component{
+    render(){
+        return <h1>This is class Component</h1>
+    }
+}
+
+export default Wellcome;
